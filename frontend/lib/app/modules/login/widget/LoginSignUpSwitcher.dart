4216dc561c4e9c995fd15c0d389f9f8dc474ab6d
Jih15 <99134563+Jih15@users.noreply.github.com>
2025-07-15 21:37:46 +0700

    feat: Adding login page for mobile UI

---
+++ frontend/lib/app/modules/login/widget/LoginSignUpSwitcher.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginSignUpSwitcher extends StatelessWidget {
+  final RxBool isLogin;
+  final Function(bool) onToggle;
+  final double width;
+
+  const LoginSignUpSwitcher({
+    super.key,
+    required this.isLogin,
+    required this.onToggle,
+    this.width = 300,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return Container(
+        width: width,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          color: Colors.grey[300],
+        ),
+        child: Stack(
+          children: [
+            AnimatedAlign(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              alignment:
+              isLogin.value ? Alignment.centerLeft : Alignment.centerRight,
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Container(
+                  width: width * 0.45,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => onToggle(true),
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color:
+                          isLogin.value ? Colors.black : Colors.grey[600],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => onToggle(false),
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color:
+                          isLogin.value ? Colors.grey[600] : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
